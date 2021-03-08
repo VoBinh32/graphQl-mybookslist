8e616ddca666533aa058932df0a81e007dbaa59c
Thanh Bình Võ <thanhbinh322000@gmail.com>
2021-03-08 17:46:40 +0200

    add mongodb and mutation

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 const schema = require("./schema/schema");
 
 const app = express();
+
+mongoose
+  .connect(keys.mongoURI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((e) => console.log(e.message));
 
 // bind express with graphql
 app.use(
